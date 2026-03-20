e4106ac1487a0a0415968e2b46f8fc68dfaf8d01
regina0714 <217802070+regina0714@users.noreply.github.com>
2026-03-20 18:19:03 -0500

    configuracion tipo 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,14 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
 
+    // Supabase
+    implementation(platform(libs.supabase.bom))
+    implementation(libs.supabase.postgrest)
+    implementation(libs.supabase.auth)
+    implementation(libs.supabase.realtime)
+    implementation(libs.supabase.storage)
+    implementation(libs.ktor.client.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
